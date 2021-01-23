ebd3fa23fcd2f2038efaa2c148498c27fa17b44a
RuthiLalhundiki <77824382+RuthiLalhundiki@users.noreply.github.com>
2021-01-23 13:53:08 +0530

    Assignment 2 Submitted

---
+++ Assignment 2/Question 2.js
@@ -0,0 +1,18 @@
+// Javascript program to find the sum of all elements in an array
+
+var arr=[34, 45, 56];
+
+arr[3]=arr[0]+arr[1]+arr[2];
+
+console.log("1st number:");
+console.log(arr[0]);
+
+console.log("2st number:");
+console.log(arr[1]);
+
+console.log("3st number:");
+console.log(arr[2]);
+
+console.log("Sum of three numbers:");
+console.log(arr[3]);
+
